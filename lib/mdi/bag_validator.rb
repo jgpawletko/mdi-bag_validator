0efc3af134ece5b86abbb2498e517e7c5a55b886
jgpawletko <jgpawletko@gmail.com>
2015-02-21 14:42:10 -0500

    initial commit

---
+++ lib/mdi/bag_validator.rb
@@ -0,0 +1,7 @@
+require "mdi/bag_validator/version"
+
+module Mdi
+  module BagValidator
+    # Your code goes here...
+  end
+end
---
+++ lib/mdi/bag_validator/version.rb
@@ -0,0 +1,5 @@
+module Mdi
+  module BagValidator
+    VERSION = "0.0.1"
+  end
+end
