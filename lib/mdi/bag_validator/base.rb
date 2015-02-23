a2571fd9439fa2e429aeff4c19ecc1c9efd773c2
jgpawletko <jgpawletko@gmail.com>
2015-02-22 21:55:24 -0500

    fix local worker configuration

--- lib/mdi/bag_validator/base.rb
+++ lib/mdi/bag_validator/base.rb
@@ -5,10 +5,21 @@ module Mdi
     class Base
       include Sneakers::Worker
       from_queue 'bag_validation'
+      opts = {   :env => 'test',
+                 :durable => true,
+                 :ack => true,
+                 :threads => 1,
+                 :prefetch => 1,
+                 :timeout_job_after => 100000000,
+                 :exchange => 'dummy',
+                 :heartbeat_interval => 5
+             }
+      Sneakers.configure(opts)
 
       def work(msg)
-        worker_trace "received #{msg}"
+        puts "received #{msg}"
         ack!
+
       end
     end
   end
